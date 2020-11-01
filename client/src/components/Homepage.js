cf1b33b72759952aa6235f507a5d9f2f867ec6a1
pDylanSmith <dylan@alignsoft.ca>
2020-10-31 20:30:29 -0400

    nav/mobile nav, my work section

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,19 +2,53 @@ import React from "react";
 
 import '../components-styles/Homepage.scss';
 
+import ProjectCard from './ProjectCard';
+
+import waygoThumbnail from '../resources/waygo-thumbnail.png';
+
 function Homepage(props) {
 
 	return (
 		<div>
-			<div className="parallax-image">
+		<section className="landing-image-section">
+			<div className="parallax-image full-screen">
 				<div className="image-section-container center">
 					<h1 className="image-header">Dylan Smith</h1>
+					<br />
 					<h3 className="image-info">Self-learning software engineer learning software and electrical engineering through research & development.</h3>
 				</div>
 			</div>
-			<div className="my-work">
+		</section>
+
+		<section className="my-work">
+			<div className="container-center">
+				<div className="my-work-title">
+					<a>My Work</a>
+				</div>
+				<div className="my-work-description">
+					<a>
+						My curiosity and eagerness fuels my motivation to build with software and engineering.
+						From designing artifical intelligence models to building electric skateboards, my passion is unrivaled in building interesting ideas and learning everything in the process.
+					</a>
+				</div>
+
+				<div className="my-work-projects">
+					<ProjectCard 
+						className="project-card"
+						link={"/projects"}  
+						image={waygoThumbnail}
+						title="Waygo Navigation App" 
+						category="Web/Mobile App" 
+						description="Waygo is a navigation app allowing users to 
+						plan optimal routes, visualize traffic, 
+						find nearby transit, and much more."
+					/>
+				</div>
 
 			</div>
+		</section>
+			
+			
 		</div>
 	)
 }
--- client/src/components/ProjectCard.js
+++ client/src/components/ProjectCard.js
@@ -1,17 +1,29 @@
 import { React } from 'react';
-import { Link } from 'react-rouder-dom';
-import './ProjectCard.scss';
+import { Link } from 'react-router-dom';
+
+import '../components-styles/ProjectCard.scss';
+
+/**
+ * Props:
+ * link=
+ * image=
+ * title=
+ * category=
+ * description=
+ * 
+ * @param {} props 
+ */
 
 function ProjectCard(props) {
 	return (
 		<div className="project-card">
-			<Link to="">
+			<Link to={ props.link }>
 				<div className="project-image">
-					<img src="" alt="Project Image" />
+					<img className="project-image" src={ props.image } alt="Project Image" />
 				</div>
-				<div className="project-title"></div>
-				<div className="project-category"></div>
-				<div className="project-description"></div>
+				<div className="project-title">{ props.title }</div>
+				<div className="project-category">{ props.category }</div>
+				<div className="project-description">{ props.description }</div>
 			</Link>
 		</div>
 	)
