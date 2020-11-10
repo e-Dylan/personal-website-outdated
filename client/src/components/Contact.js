1c72b44cf44cef8529e59e13ddac0eb12fe517a5
pDylanSmith <dylan@alignsoft.ca>
2020-11-10 14:53:00 -0500

    contact section design

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -4,40 +4,45 @@ import '../components-styles/Contact.scss';
 
 function Contact(props) {
 	return (
-		<div className="container-center">
-			<div className="section-title">
-				<a>CONTACT ME</a>
-				<div className="section-title-underline"></div>
-			</div>
+		<div>
+			<svg className="contact-footer-svg" preserveAspectRatio="none" viewBox="0 0 100 102" height="75" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" class="svgcolor-light">
+				<path d="M0 0 L50 100 L100 0 Z" fill="white" stroke="white"></path>
+			</svg>
+			<div className="container-center">
+				<div className="section-title" id="contact-section-title">
+					<a>CONTACT ME</a>
+					<div className="section-title-underline"></div>
+				</div>
 
-			<div className="contact-area">
-				<div className="contact-form">
-					<div className="form-header">
-						Have any questions or want to get in contact with me?
+				<div className="contact-area">
+					<div className="contact-form">
+						<div className="form-header">
+							Have any questions or want to get in contact with me?
+						</div>
+						<input 
+							className="contact-input"
+							placeholder="Enter your name"
+						></input>
+						<input
+							type="email" 
+							className="contact-input"
+							placeholder="Enter a valid email address"
+						></input>
+						<textarea 
+							type="text"
+							className="contact-text-area"
+							placeholder="Enter your message"
+						></textarea>
+						<button className="submit-button">Submit</button>
 					</div>
-					<input 
-						className="contact-input"
-						placeholder="Enter your name"
-					></input>
-					<input
-						type="email" 
-						className="contact-input"
-						placeholder="Enter a valid email address"
-					></input>
-					<textarea 
-						type="text"
-						rows="3"
-						className="contact-input"
-						placeholder="Enter your message"
-					></textarea>
-					<button className="submit-button">Submit</button>
-				</div>
-				<div className="my-info">
+					<div className="my-info">
 
+					</div>
 				</div>
+				
 			</div>
-			
 		</div>
+		
 	)
 }
 
