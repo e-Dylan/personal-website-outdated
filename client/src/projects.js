812616a3d0bfc403c61e09d480ba327b42125ad3
e-Dylan <dylan@alignsoft.ca>
2020-12-06 22:15:29 -0500

    additional projects

--- client/src/projects.js
+++ client/src/projects.js
@@ -1,6 +1,9 @@
 import waygoThumbnail from './resources/project_thumbnails/waygo-thumbnail.png';
 import speednetThumbnail from './resources/project_thumbnails/speed-net-thumbnail.png';
 import weatherAppThumbnail from './resources/project_thumbnails/weather-app-thumbnail.png';
+import faceGeneratorThumbnail from './resources/project_thumbnails/face-generator-thumbnail.gif';
+import faceAnimatorThumbnail from './resources/project_thumbnails/face-animator-thumbnail.gif';
+import sortingAlgorithmThumbnail from './resources/project_thumbnails/sorting-algorithm-thumbnail.gif';
 
 const projects = {
 	waygo: {
@@ -11,6 +14,26 @@ const projects = {
 		description: "Waygo is a navigation app allowing users to plan optimal routes, visualize traffic, find nearby transit, and much more.",
 		"date": "06/12/2020 - Current",
 		"github": "https://github.com/e-Dylan/waygo",
+		"live-demo": "",
+	},
+	faceGenerator: {
+		title: "Face Generation Neural Network",
+		image: faceGeneratorThumbnail,
+		category: "Machine Learning Algorithm" ,
+		tech: "Python, PyTorch, Colab",
+		description: "Face generation software trained using a generative adversarial network (GAN) algorithm, trained on human face datasets.",
+		"date": "11/26/2020",
+		"github": "https://github.com/e-Dylan/gan_facegenerator",
+		"live-demo": "",
+	},
+	faceAnimator: {
+		title: "Cartoon/Animating Neural Network",
+		image: faceAnimatorThumbnail,
+		category: "Machine Learning Algorithm" ,
+		tech: "Python, PyTorch, Colab",
+		description: "Generates artifical human faces using a GAN algorithm, cartoonizes them, and animates them all using AI.",
+		"date": "12/01/2020",
+		"github": "https://github.com/e-Dylan/gan_faceanimator",
 		"live-demo": "",
 	},
 	speednet: {
@@ -31,6 +54,16 @@ const projects = {
 		description: "Small weather app widget with animation and icons to be displayed in a browser or mobile app.",
 		date: "02/21/2017",
 		"github": "https://github.com/e-Dylan/simple-weather-app",
+		"live-demo": "",
+	},
+	sorting_algorithms: {
+		title: "Sorting Algorithm Visualizer",
+		image: sortingAlgorithmThumbnail,
+		category: "Java Program",
+		tech: "Java, JavaFX",
+		description: "Sorting algorithm implementations in Java with a simple JavaFX GUI.",
+		date: "10/15/2019",
+		"github": "https://github.com/e-Dylan/",
 		"live-demo": "",
 	},
 	
